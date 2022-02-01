6b9952d192bf39b0e56b3f1de6eabe824beb0fb6
ch1lly <calhill404@gmail.com>
2022-02-01 13:50:26 +0000

    added guess limit of 5 guesses

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,14 +9,21 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     int guess;
     int number = (int) (Math.random() * 100) + 1;
+    int guessLimit = 5;
     System.out.println("Please guess a number between 1 and 100");
+    System.out.println("You have 5 attempts");
 
+    int currentGuess = 1;
     do {
-      guess = scanner.nextInt();
-      if (number == guess)
-        System.out.println("Well done, the number was " + number);
-       else if (number != guess) System.out.println("Please try again");
-    } while (guess != number);
 
+      guess = scanner.nextInt();
+      if (number == guess) System.out.println("Well done, the number was " + number);
+      else if (currentGuess < guessLimit)
+        System.out.println("Please try again");
+      currentGuess++;
+    } while ((guess != number) && (currentGuess <= guessLimit));
+    if (currentGuess > guessLimit) {
+      System.out.println("Too many guesses, you lose! " + "The correct number was: " + number);
+    }
   }
 }
