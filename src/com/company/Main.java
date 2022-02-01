15050bc9505ad49eb32a335cde991f9ccb878a96
ch1lly <calhill404@gmail.com>
2022-02-01 12:33:43 +0000

    files added

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    Scanner scanner = new Scanner(System.in);
+    int guess;
+    int number = (int) (Math.random() * 10) + 1;
+    System.out.println("Please guess a number between 1 and 10");
+
+    do {
+      guess = scanner.nextInt();
+      if (number == guess)
+        System.out.println("Well done, the number was " + number);
+       else if (number != guess) System.out.println("Please try again");
+    } while (guess != number);
+
+  }
+}
